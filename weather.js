93048666f632a57aa7e97703445e50ff41d4aaa0
donkovtsev <ag.donkovtcev@severstal.com>
2022-01-06 14:05:23 +0300

    save token

--- weather.js
+++ weather.js
@@ -1,14 +1,24 @@
 #!/usr/bin/env node
 
 import { getArgs } from "./helpers/args.js";
-import { printHelp } from "./services/log.service.js";
+import { printHelp, printSuccess, printError } from "./services/log.service.js";
+import { saveKeyValue } from "./services/storage.service.js";
 
-const initCLI = () => {
-  console.log("started");
+const saveToken = async (token) => {
+  if (!token.length) {
+    return printError("Токен не передан");
+  }
+  try {
+    await saveKeyValue("token", token);
+    printSuccess("Токен сохранен");
+  } catch (e) {
+    printError(e.message);
+  }
+};
 
+const initCLI = async () => {
   const args = getArgs(process.argv);
 
-  console.log(args);
   if (args.h) {
     // show help
     printHelp();
@@ -20,6 +30,7 @@ const initCLI = () => {
 
   if (args.t) {
     // save token
+    return await saveToken(args.t);
   }
 };
 
